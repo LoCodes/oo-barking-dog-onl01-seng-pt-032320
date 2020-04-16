cd10a36f1de20b40e58fb057af8a6183e0abb319
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-16 17:27:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,13 +14,13 @@ class Dog
     @dogs_name
   end
 
-  # def  bark
-  #   puts "woof!"
-  # end
+  def  bark
+    puts "woof!"
+  end
 end
 
 fido = Dog.new
 fido.name = "Fido"
 fido.name
-# 
-# fido.bark
+
+fido.bark
